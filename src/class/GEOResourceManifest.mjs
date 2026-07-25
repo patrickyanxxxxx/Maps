886cf02423558d04341b46cddd9d9a6829d0b1b5
patrickyanxxxxx <150406461+patrickyanxxxxx@users.noreply.github.com>
2026-07-26 05:57:30 +0800

    feat: append module version to the Adaptive Hybrid attribution label

--- src/class/GEOResourceManifest.mjs
+++ src/class/GEOResourceManifest.mjs
@@ -720,7 +720,7 @@ export default class GEOResourceManifest {
 			.map((attribution, index) => {
 				switch (attribution.name) {
 					case "‎":
-						attribution.name = ` iRingo: 📍 Adaptive Hybrid\n${new Date().toISOString().replace("T", " ").replace(/\.\d+Z$/, " UTC")}`;
+						attribution.name = ` iRingo: 📍 Adaptive Hybrid v6.4.1\n${new Date().toISOString().replace("T", " ").replace(/\.\d+Z$/, " UTC")}`;
 						delete attribution.plainTextURLSHA256Checksum;
 						break;
 					case "AutoNavi":
